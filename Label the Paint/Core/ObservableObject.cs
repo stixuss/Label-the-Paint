6cf1ca20e68f995cf4a45c93abd0bebc974be92e
Pawel Sochon <sochon.p@gmail.com>
2021-10-17 21:36:58 +0200

    Add project files.

---
+++ Label the Paint/Core/ObservableObject.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+namespace Label_the_Paint.Core
+{
+    class ObservableObject : INotifyPropertyChanged
+
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ Label the Paint/Core/RelayCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Label_the_Paint.Core
+{
+    class RelayCommand : ICommand
+    {
+        private Action<object> _execute;
+        private Func<object, bool> _canExectute;
+        
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            _execute = execute;
+            _canExectute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExectute == null || _canExectute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
